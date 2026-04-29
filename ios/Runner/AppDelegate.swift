2012e3d401fb3f2a7d16ef78ba8d09b2ea68cd3d
kate <vietlovedating@gmail.com>
2026-04-29 22:17:04 +1000

    Fix iOS AppDelegate launch setup

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -9,12 +9,8 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
 
-    window = UIWindow(frame: UIScreen.main.bounds)
-    window?.rootViewController = FlutterViewController()
-    window?.makeKeyAndVisible()
-
     GeneratedPluginRegistrant.register(with: self)
 
-    return true
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
