f59427aa3eba3fba1afe3e078103164d1a080a63
kate <vietlovedating@gmail.com>
2026-05-03 19:29:32 +1000

    Restore working Firebase plist

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,13 +3,27 @@ import Flutter
 import FirebaseCore
 import FirebaseAuth
 
-@UIApplicationMain
+@main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    FirebaseApp.configure()
+
+    if FirebaseApp.app() == nil {
+      let options = FirebaseOptions(
+        googleAppID: "1:358333936335:ios:b4f070eb11719b4d54fdba",
+        gcmSenderID: "358333936335"
+      )
+
+      options.apiKey = "AIzaSyD5xBmU_Qpr4PsCOYRsz6Ldjh4wIyl3zi4"
+      options.projectID = "flutter-vietlove-dating"
+      options.storageBucket = "flutter-vietlove-dating.firebasestorage.app"
+      options.clientID = "358333936335-ipsfdl5slhmp1bia9nh02rl702qa62o2.apps.googleusercontent.com"
+
+      FirebaseApp.configure(options: options)
+    }
+
     GeneratedPluginRegistrant.register(with: self)
 
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
