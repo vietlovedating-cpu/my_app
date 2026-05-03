54c75bc1ae43ca6e9d37cfb0dbce832e80ddd177
kate <vietlovedating@gmail.com>
2026-05-03 20:48:07 +1000

    Remove duplicate native Firebase init

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -10,20 +10,6 @@ import FirebaseAuth
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
 
-    if FirebaseApp.app() == nil {
-      let options = FirebaseOptions(
-        googleAppID: "1:358333936335:ios:b4f070eb11719b4d54fdba",
-        gcmSenderID: "358333936335"
-      )
-
-      options.apiKey = "AIzaSyD5xBmU_Qpr4PsCOYRsz6Ldjh4wIyl3zi4"
-      options.projectID = "flutter-vietlove-dating"
-      options.storageBucket = "flutter-vietlove-dating.firebasestorage.app"
-      options.clientID = "358333936335-ipsfdl5slhmp1bia9nh02rl702qa62o2.apps.googleusercontent.com"
-
-      FirebaseApp.configure(options: options)
-    }
-
     GeneratedPluginRegistrant.register(with: self)
 
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
